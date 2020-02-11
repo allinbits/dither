32cafb685e6b28833a76f5d374b822d53b59a3eb
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-02-12 05:08:40 +0800

    update page title to alpha

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .page-home
-  app-header(page-title="Dither")
+  app-header(page-title="Dither Alpha")
     template(v-if="userSignedIn")
       btn-icon(slot="btn-left" type="link" :to="{ name: 'settings' }" icon="settings")
     template(v-if="userSignedIn")
