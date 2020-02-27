d5a34a0a330b0781578d349e598c2da97a8e412e
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-02-27 11:36:32 -0500

    update wallet message

--- client/src/views/Wallet.vue
+++ client/src/views/Wallet.vue
@@ -10,8 +10,11 @@
 
   section-default(v-if="settings && settings.data && settings.data.wallet")
     div(slot="section-title") Your wallet
-    p(v-if="tokens") You need ATOM tokens to post messages on Dither. You currently have enough!
-    p(v-else) You need ATOM tokens to post messages on Dither, but you don't have any! You can get some by messaging #[a(:href="tweetUrl" target="_blank" rel="noopener noreferrer") @VirgoUDV on Twitter].
+
+    template(v-if="tokens")
+      p You currently have enough ATOM to post messages on Dither.
+    template(v-else)
+      p You need ATOM to post messages on Dither, but you don't have any! You should send some ATOM (0.05 is enough) to the address below. You can get a small amount by messaging #[a(:href="tweetUrl" target="_blank" rel="noopener noreferrer") @VirgoUDV].
 
     p
       .break-address This is your wallet address:
