3ef3d5ad2088ca780e0a7e038dfa30fc4e44c8d9
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-02-29 08:22:49 -0500

    increase cost of txs

--- client/src/scripts/tx.js
+++ client/src/scripts/tx.js
@@ -44,7 +44,7 @@ async function sendTx(params) {
   // if this tx doesn't send tokens, send one uatom to this address
   if (!params.amount) {
     params.to = store.state.blockchain.toAddress;
-    params.amount = "1";
+    params.amount = "20000";
   }
 
   let tx = defaultTx(
@@ -120,92 +120,6 @@ async function sendTx(params) {
     };
     return queuedTxSend;
   }
-}
-
-async function sendTxOld(fromAddr, type, parentAddr, memo, toAddr, amount) {
-  let txMemo, txToAddr, txAmount;
-  let walletData = store.state.settings.data.wallet;
-
-  // set up the account details
-  let account = await fetch(
-    `${store.state.blockchain.lcd}/auth/accounts/${fromAddr}`
-  );
-  let accountJson = await account.json();
-  // console.log("account info", accountJson.result);
-
-  if (type === "send") {
-    txMemo = memo;
-    txToAddr = toAddr;
-    txAmount = amount;
-  } else {
-    txMemo = h.getMemoPrefix(type, parentAddr) + memo;
-    txToAddr = store.state.blockchain.toAddress;
-    // assuming ATOM price of $5 USD, charge 10c per transaction
-    txAmount = "20000";
-  }
-
-  let tx = defaultTx(
-    store.state.blockchain.defaultGas,
-    fromAddr,
-    txMemo,
-    txToAddr,
-    txAmount
-  );
-
-  // set the sequence to be the current account sequence plus any queued memos
-  let accountSequence = accountJson.result.value.sequence;
-  let queuedMemosLength = Object.keys(store.state.queuedMemos).length;
-  let queuedTxSendsLength = Object.keys(store.state.queuedTxSends).length;
-  let currentSequence =
-    parseInt(accountSequence) +
-    parseInt(queuedMemosLength) +
-    parseInt(queuedTxSendsLength);
-  currentSequence = currentSequence.toString();
-  // console.log("queuedMemosLength", queuedMemosLength);
-  // console.log("accountSequence", accountSequence);
-  // console.log("currentSequence", currentSequence);
-
-  let signMeta = {
-    account_number: accountJson.result.value.account_number,
-    chain_id: store.state.blockchain.chainId,
-    sequence: currentSequence
-  };
-
-  let wallet = {
-    address: fromAddr,
-    privateKey: Uint8Array.from(walletData.privateKey),
-    publicKey: Uint8Array.from(walletData.publicKey)
-  };
-
-  // prepare the transaction for sending
-  const txSigned = signTx(tx, signMeta, wallet);
-  const txBroadcast = createBroadcastTx(txSigned, "sync");
-
-  // send tx and await results
-  let txResponse = await fetch(`${store.state.blockchain.lcd}/txs`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(txBroadcast)
-  });
-  let txResponseJson = await txResponse.json();
-
-  let queuedMemo = {
-    id: txResponseJson.txhash,
-    address: fromAddr,
-    height: 0,
-    memo: tx.memo,
-    parent: parentAddr,
-    response: txResponseJson,
-    timestamp: new Date().toISOString(),
-    tx: txBroadcast,
-    type: type,
-    reposts: 0,
-    likes: 0,
-    comments: 0
-  };
-  return queuedMemo;
 }
 
 export default { sendTx, sendTxOld };
