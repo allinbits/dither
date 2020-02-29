f32ea66c4cb91f860bb77a618e3d592e4dd4546e
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-02-29 11:51:37 -0500

    update fee

--- client/src/scripts/tx.js
+++ client/src/scripts/tx.js
@@ -1,6 +1,8 @@
 import { signTx, createBroadcastTx } from "@tendermint/sig";
 import store from "../store/index.js";
 
+const fee = "2000";
+
 function defaultTx(from, to, amount, memo, gas) {
   return {
     fee: {
@@ -43,7 +45,7 @@ async function sendTx(params) {
   // if this tx doesn't send tokens, send one uatom to this address
   if (!params.amount) {
     params.to = store.state.blockchain.toAddress;
-    params.amount = "20000";
+    params.amount = "2000";
   }
 
   let tx = defaultTx(
@@ -91,7 +93,7 @@ async function sendTx(params) {
 
   console.log("tx params", params);
 
-  if (params.amount === "20000") {
+  if (params.amount === "2000") {
     let queuedMemo = {
       id: txResponseJson.txhash,
       address: params.from,
