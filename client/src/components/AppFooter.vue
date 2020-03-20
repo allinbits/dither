2bcd711ee20c3a07798417f679f76499d803adae
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-03-20 15:26:17 -0400

    enable notifications page

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -3,8 +3,8 @@
   .app-footer__container
     btn-icon(type="link" icon="home" :to="{ name: 'home' }")
     btn-icon(type="link" icon="hash" :to="{ name: 'channels' }")
-    // btn-icon(type="link" icon="globe" :to="{ name: 'memos' }")
     btn-icon(type="link" icon="users" :to="{ name: 'accounts' }")
+    btn-icon(type="link" icon="bell" :to="{ name: 'notifications' }")
     btn-icon(type="link" icon="dollar-sign" :to="{ name: 'wallet' }")
 </template>
 
